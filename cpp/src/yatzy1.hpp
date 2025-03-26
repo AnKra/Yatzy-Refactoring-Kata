63ede57341114b71cf2c3456c46f3be27dc1c5ec
Kramer Angela (XC-AS/EDE4) <Angela.Kramer@de.bosch.com>
2025-03-26 09:51:52 +0100

    reformat source files

--- cpp/src/yatzy1.hpp
+++ cpp/src/yatzy1.hpp
@@ -4,30 +4,29 @@
 class Yatzy1
 {
 public:
-
-    static int Chance(int d1, int d2, int d3, int d4, int d5);
-    static int yatzy(int dice[]);
-    static int Ones(int d1, int d2, int d3, int d4, int d5);
-    static int Twos(int d1, int d2, int d3, int d4, int d5);
-    static int Threes(int d1, int d2, int d3, int d4, int d5);
+   static int Chance(int d1, int d2, int d3, int d4, int d5);
+   static int yatzy(int dice[]);
+   static int Ones(int d1, int d2, int d3, int d4, int d5);
+   static int Twos(int d1, int d2, int d3, int d4, int d5);
+   static int Threes(int d1, int d2, int d3, int d4, int d5);
 
 protected:
-    int * dice;
+   int* dice;
+
 public:
-    Yatzy1();
-    Yatzy1(int d1, int d2, int d3, int d4, int _5);
-    int Fours();
-    int Fives();
-    int sixes();
-    int ScorePair(int d1, int d2, int d3, int d4, int d5);
-    static int TwoPair(int d1, int d2, int d3, int d4, int d5);
-    static int FourOfAKind(int _1, int _2, int d3, int d4, int d5);
-    static int ThreeOfAKind(int d1, int d2, int d3, int d4, int d5);
+   Yatzy1();
+   Yatzy1(int d1, int d2, int d3, int d4, int _5);
+   int        Fours();
+   int        Fives();
+   int        sixes();
+   int        ScorePair(int d1, int d2, int d3, int d4, int d5);
+   static int TwoPair(int d1, int d2, int d3, int d4, int d5);
+   static int FourOfAKind(int _1, int _2, int d3, int d4, int d5);
+   static int ThreeOfAKind(int d1, int d2, int d3, int d4, int d5);
 
-    static int SmallStraight(int d1, int d2, int d3, int d4, int d5);
-    static int LargeStraight(int d1, int d2, int d3, int d4, int d5);
-    static int FullHouse(int d1, int d2, int d3, int d4, int d5);
-
+   static int SmallStraight(int d1, int d2, int d3, int d4, int d5);
+   static int LargeStraight(int d1, int d2, int d3, int d4, int d5);
+   static int FullHouse(int d1, int d2, int d3, int d4, int d5);
 };
 
 #endif
